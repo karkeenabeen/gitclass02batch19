dbd611ef8b42912e78f9b25c4789dd4692bd8a99
nabin karki <karkeenabeen@gmail.com>
2024-03-13 20:40:47 -0400

    third commit

--- src/main/java/org/example.java
+++ src/main/java/org/example.java
@@ -4,5 +4,6 @@ public class example {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello mars");
+        System.out.println("helooooooooo");
     }
 }
